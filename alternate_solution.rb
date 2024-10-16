aa8307e057f2c46708d14a4a111ee8600fd2787c
taramakhija51 <114837329+taramakhija51@users.noreply.github.com>
2024-10-16 00:32:14 +0000

    functional completion of objectives

--- alternate_solution.rb
+++ alternate_solution.rb
@@ -16,9 +16,11 @@ lat = northeast.fetch("lat")
 lng = northeast.fetch('lng')
 pirate_weather_api_key = "3RrQrvLmiUayQ84JSxL8D2aXw99yRKlx1N4qFDUE"
 pirate_url = "https://api.pirateweather.net/forecast/#{pirate_weather_api_key}/#{lat},#{lng}"
-print pirate_url
 x = HTTP.get(pirate_url)
 parsed_response = JSON.parse(x)
 currently_hash = parsed_response.fetch("currently")
 current_temp = currently_hash.fetch("temperature")
+minute_hash = parsed_response.fetch("minutely")
+minute_temp = minute_hash.fetch("summary")
 puts "The current temperature is " + current_temp.to_s + "."
+puts "The weather for the next hour is #{minute_temp}."
