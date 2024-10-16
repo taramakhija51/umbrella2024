dd7928f5adc609733b8756be0a3d76a999064a4c
taramakhija51 <114837329+taramakhija51@users.noreply.github.com>
2024-10-16 00:24:13 +0000

    current temperature output

---
+++ alternate_solution.rb
@@ -0,0 +1,24 @@
+require "http"
+require "json"
+
+puts "What city are you based in?"
+location = gets
+location = location.gsub(" ", "%20")
+url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{location}&key=AIzaSyDKz4Y3bvrTsWpPRNn9ab55OkmcwZxLOHI"
+response = HTTP.get(url)
+parsed_response = JSON.parse(response)
+results = parsed_response.fetch("results")
+components = results.at(0)
+components = components.fetch("geometry")
+bounds = components.fetch("bounds")
+northeast = bounds.fetch("northeast")
+lat = northeast.fetch("lat")
+lng = northeast.fetch('lng')
+pirate_weather_api_key = "3RrQrvLmiUayQ84JSxL8D2aXw99yRKlx1N4qFDUE"
+pirate_url = "https://api.pirateweather.net/forecast/#{pirate_weather_api_key}/#{lat},#{lng}"
+print pirate_url
+x = HTTP.get(pirate_url)
+parsed_response = JSON.parse(x)
+currently_hash = parsed_response.fetch("currently")
+current_temp = currently_hash.fetch("temperature")
+puts "The current temperature is " + current_temp.to_s + "."
